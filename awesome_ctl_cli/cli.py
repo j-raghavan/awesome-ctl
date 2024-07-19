d57c6dc2332220963f91ffd920a9ab6ee76068c4
Jayasimha Raghavan <mr.jayasimha@gmail.com>
2024-07-19 11:35:03 -0700

    Black formatting

--- awesome_ctl_cli/cli.py
+++ awesome_ctl_cli/cli.py
@@ -11,11 +11,20 @@ from awesome_ctl import __build_number__, __version__
 
 app = typer.Typer()
 
+
 @app.command()
-def diagnose(ctx: typer.Context,
-             connector: str = typer.Argument(..., help="The connector to use for diagnostics (e.g., 'kubernetes')"),
-             namespace: Optional[str] = typer.Option(None, "--namespace", "-n", help="The Kubernetes namespace to analyze (only for Kubernetes connector)"),
-             ):
+def diagnose(
+    ctx: typer.Context,
+    connector: str = typer.Argument(
+        ..., help="The connector to use for diagnostics (e.g., 'kubernetes')"
+    ),
+    namespace: Optional[str] = typer.Option(
+        None,
+        "--namespace",
+        "-n",
+        help="The Kubernetes namespace to analyze (only for Kubernetes connector)",
+    ),
+):
     """
     Diagnose issues using the specified connector.
     """
@@ -29,6 +38,7 @@ def diagnose(ctx: typer.Context,
     else:
         typer.echo(f"Connector '{connector}' is not yet supported.")
 
+
 @app.command()
 def list():
     """
@@ -37,23 +47,20 @@ def list():
     typer.echo("Available Connectors:")
     # ... (Logic to retrieve and print available connectors)
 
+
 @app.callback()
 def callback():
-    """ Awesome Command Line Interface to interact with Awesome Client"""
+    """Awesome Command Line Interface to interact with Awesome Client"""
     banner = figlet_format("awesome - ctl", font="standard", width=80, justify="center")
     typer.echo("")
     typer.secho(banner, fg=typer.colors.BRIGHT_WHITE, bold=True)
     typer.echo("")
     typer.secho(f"  Version:  {__version__}", fg=typer.colors.BRIGHT_BLUE, bold=True)
-    typer.secho(
-        f"  Build: {__build_number__}", fg=typer.colors.BRIGHT_BLUE, bold=True
-    )
+    typer.secho(f"  Build: {__build_number__}", fg=typer.colors.BRIGHT_BLUE, bold=True)
 
     typer.echo("")
 
-    typer.echo(
-        f"  You can access awesome-ctl at http://127.0.0.1:4200"
-    )
+    typer.echo(f"  You can access awesome-ctl at http://127.0.0.1:4200")
     typer.echo("")
 
     def run_server():
@@ -63,8 +70,10 @@ def callback():
     server_thread.daemon = True
     server_thread.start()
 
-def main(args = None):
+
+def main(args=None):
     app(args)
 
+
 if __name__ == "__main__":
-    main(args = sys.argv[1:])
+    main(args=sys.argv[1:])
